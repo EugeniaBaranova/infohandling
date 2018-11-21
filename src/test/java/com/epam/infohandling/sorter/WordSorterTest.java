2b9af7ae12fddfc94b2e40cf93f0fd635490dfac
EugeniaBaranova <21061997qwertyuiop@gmail.com>
2018-11-21 21:26:57 +0300

    commit 3

--- src/test/java/com/epam/infohandling/sorter/WordSorterTest.java
+++ src/test/java/com/epam/infohandling/sorter/WordSorterTest.java
@@ -3,8 +3,6 @@ package com.epam.infohandling.sorter;
 import com.epam.infohandling.composite.Lexeme;
 import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,10 +15,28 @@ public class WordSorterTest {
     private Lexeme firstWord = Lexeme.word("sentence");
     private Lexeme secondWord = Lexeme.word("then");
     private Lexeme thirdWord =  Lexeme.word("there");
-    private List<Lexeme> differentCharNumberLexemes = new ArrayList<>(Arrays.asList(firstWord, secondWord, thirdWord));
+    private Lexeme forthWord = Lexeme.word("here");
+    private List<Lexeme> differentCharNumberLexemes = Arrays.asList(firstWord, secondWord, thirdWord);
 
     @Test
     public void shouldSortDescendingNumberOfCharWhenNumberIsDifferent(){
+        //given
+
+        //when
+        List<Lexeme> result = wordSorter.sort(differentCharNumberLexemes, 'e');
+        //then
+        Assert.assertThat(result.size(), is(3));
+        Lexeme firstSortedLexeme = result.get(0);
+        Assert.assertThat(firstSortedLexeme, is(firstWord));
+        Lexeme secondSortedLexeme = result.get(1);
+        Assert.assertThat(secondSortedLexeme, is(thirdWord));
+        Lexeme thirdSortedLexeme = result.get(2);
+        Assert.assertThat(thirdSortedLexeme, is(secondWord));
+    }
+
+    //TODO
+    @Test
+    public void shouldSortDescendingNumberOfCharWhenNumberIsNotDifferent(){
         //given
 
         //when
