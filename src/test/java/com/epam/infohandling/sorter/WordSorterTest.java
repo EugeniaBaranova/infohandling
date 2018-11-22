544e45fc9bbda11118a3ccc071be56f1e02e6219
Kosty Kizin <KostyKizin@gmail.com>
2018-11-22 18:05:28 +0300

    added WordSorter

--- src/test/java/com/epam/infohandling/sorter/WordSorterTest.java
+++ src/test/java/com/epam/infohandling/sorter/WordSorterTest.java
@@ -10,6 +10,9 @@ import static org.hamcrest.CoreMatchers.is;
 
 public class WordSorterTest {
 
+    private static final char SORTING_SYMBOL = 'e';
+    private static final char ABSENT_SYMBOL = 'q';
+
     private WordSorter wordSorter = new WordSorter();
 
     private Lexeme firstWord = Lexeme.word("sentence");
@@ -17,13 +20,14 @@ public class WordSorterTest {
     private Lexeme thirdWord =  Lexeme.word("there");
     private Lexeme forthWord = Lexeme.word("here");
     private List<Lexeme> differentCharNumberLexemes = Arrays.asList(firstWord, secondWord, thirdWord);
+    private List<Lexeme> notDifferentCharNumberLexemes = Arrays.asList(firstWord, secondWord, thirdWord, forthWord);
+    private List<Lexeme> withoutAbsentSymbolLexemes = Arrays.asList(secondWord, thirdWord);
+
 
     @Test
     public void shouldSortDescendingNumberOfCharWhenNumberIsDifferent(){
-        //given
-
         //when
-        List<Lexeme> result = wordSorter.sort(differentCharNumberLexemes, 'e');
+        List<Lexeme> result = wordSorter.sort(differentCharNumberLexemes, SORTING_SYMBOL);
         //then
         Assert.assertThat(result.size(), is(3));
         Lexeme firstSortedLexeme = result.get(0);
@@ -34,20 +38,39 @@ public class WordSorterTest {
         Assert.assertThat(thirdSortedLexeme, is(secondWord));
     }
 
-    //TODO
     @Test
     public void shouldSortDescendingNumberOfCharWhenNumberIsNotDifferent(){
-        //given
-
         //when
-        List<Lexeme> result = wordSorter.sort(differentCharNumberLexemes, 'e');
+        List<Lexeme> result = wordSorter.sort(notDifferentCharNumberLexemes, SORTING_SYMBOL);
         //then
-        Assert.assertThat(result.size(), is(3));
+        Assert.assertThat(result.size(), is(4));
         Lexeme firstSortedLexeme = result.get(0);
         Assert.assertThat(firstSortedLexeme, is(firstWord));
         Lexeme secondSortedLexeme = result.get(1);
-        Assert.assertThat(secondSortedLexeme, is(thirdWord));
+        Assert.assertThat(secondSortedLexeme, is(forthWord));
         Lexeme thirdSortedLexeme = result.get(2);
-        Assert.assertThat(thirdSortedLexeme, is(secondWord));
+        Assert.assertThat(thirdSortedLexeme, is(thirdWord));
+        Lexeme forthSortedLexeme = result.get(3);
+        Assert.assertThat(forthSortedLexeme, is(secondWord));
+    }
+
+    @Test
+    public void shouldNotSortAndReturnEmptyListWhenGivenNullList(){
+        //when
+        List<Lexeme> result = wordSorter.sort(null, SORTING_SYMBOL);
+        //then
+        Assert.assertThat(result.size(), is(0));
+    }
+
+    @Test
+    public void shouldNotSortAndReturnTheSameListWhenSymbolIsAbsent(){
+        //when
+        List<Lexeme> result = wordSorter.sort(withoutAbsentSymbolLexemes, ABSENT_SYMBOL);
+        //then
+        Assert.assertThat(result.size(), is(2));
+        Lexeme firstSortedLexeme = result.get(0);
+        Assert.assertThat(firstSortedLexeme, is(secondWord));
+        Lexeme secondSortedLexeme = result.get(1);
+        Assert.assertThat(secondSortedLexeme, is(thirdWord));
     }
 }
