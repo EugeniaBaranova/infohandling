eb5489b0c5c9a30b10345c19c3a14b9e2861cd23
EugeniaBaranova <21061997qwertyuiop@gmail.com>
2018-11-26 21:31:35 +0300

    added ComponentServiceTest

---
+++ src/main/java/com/epam/infohandling/service/sorter/comparator/WordLengthComparator.java
@@ -0,0 +1,12 @@
+package com.epam.infohandling.service.sorter.comparator;
+
+import com.epam.infohandling.entity.composite.Component;
+
+import java.util.Comparator;
+
+public class WordLengthComparator implements ComponentComparator{
+    @Override
+    public Comparator<Component> compare() {
+        return null;
+    }
+}
